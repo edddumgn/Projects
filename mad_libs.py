52ac1fb322c4ae1aee0c4ebed322dd3e7b7cbe95
edddumgn <87732656+edddumgn@users.noreply.github.com>
2022-08-23 23:07:23 -0300

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,7 @@
+adj = input("Adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb two: ")
+famous_person = input("name of famous person: ")
+MadLib = f"Computer programming is so {adj}! Its make me so excited all the time because i love to {verb1}. \
+Stay hydrated and {verb2} like you are {famous_person}!" 
+print(MadLib)
